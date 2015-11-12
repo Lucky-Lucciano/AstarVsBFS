2d1d7ccdc9bc8c450b3404787ab857273967129c
Ays <luka_t@hotmail.de>
2015-11-12 01:06:31 +0100

    Resumed BFS implementation

--- src/com/ai/algorithms/search/BreadthFirst.java
+++ src/com/ai/algorithms/search/BreadthFirst.java
@@ -5,19 +5,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.etfbl.ui.Grid.GridDisplay;
 import net.etfbl.ui.elements.GridField;
+import com.ai.algorithms.utility.Processing;
+
 
 public class BreadthFirst {
-	
+	private static GridDisplay grid;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		recurBreadthFirst(new GridField(2, 2));
 	}
 	
 	public static void recurBreadthFirst(GridField node) {
 		List<GridField> que = new ArrayList<GridField>();
+		List<GridField> history = new ArrayList<GridField>();
 		Map<GridField , Integer> distance = new HashMap<GridField, Integer>();
 		Map<GridField , GridField> cameFrom = new HashMap<GridField, GridField>();
 		boolean found = false;
@@ -29,6 +32,26 @@ public class BreadthFirst {
 		
 		while(que.size() > 0 && !found) {
 			GridField currentNode = que.remove(0);
+			List<GridField> frontier = Processing.getFrontLine(grid, currentNode);
+			//GridField nodeBeingChecked;
+			
+			history.add(currentNode);
+			
+			// Insert into field current node value
+			// higlightCurrentNode();
+			
+			for (GridField gridField : frontier) {
+				if(gridField.equals("GOAL NODE RowXColumn")) {
+					//highlightGoal
+					found = true;
+					
+					distance.put(gridField, distance.get(node) + 1);
+					cameFrom.put(gridField, node);
+					//createGoalRoad();
+				}
+			}
+			
+			
 			
 			System.out.println(currentNode);
 		}
